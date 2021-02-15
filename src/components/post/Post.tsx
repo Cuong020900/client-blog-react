9ca2375efe3c935b9cb01619c2681871d931cd4c
cuongtq37 <trancuong.02092000@gmail.com>
2021-02-15 21:58:30 +0700

    change css

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -12,14 +12,14 @@ import Showdown from "showdown";
 import showdownToc from "showdown-toc";
 import MDEditor from '@uiw/react-md-editor';
 
-function Post (props: any) {
+function Post(props: any) {
     const [title, setTitle] = useState('Không có title')
     const [content, setContent] = useState("Không có nội dung")
     const [content2, setContent2] = useState("Không có nội dung")
     const [userInfo, setUserInfo] = useState("Trần Quốc Cường")
     const [tocHTML, setTocHTML] = useState<any>(null)
-    const toc:any[] = [];
-    const showdown = new Showdown.Converter({ extensions: [showdownToc({ toc })] });
+    const toc: any[] = [];
+    const showdown = new Showdown.Converter({extensions: [showdownToc({toc})]});
     let result: any;
 
 
@@ -57,7 +57,7 @@ function Post (props: any) {
                     let b = document.createElement("li")
                     b.classList.add("content-outline__item", "content-outline__item--level-2")
                     let link = document.createElement("a")
-                    link.classList.add("link", "link-active", "index")
+                    link.classList.add("link", "index")
                     link.innerText = e.text
                     link.href = '#' + e.anchor
                     switch (e.level) {
@@ -69,8 +69,7 @@ function Post (props: any) {
                             b.classList.add("child-path-lv-3")
                             break
                         }
-                        case 4:
-                        {
+                        case 4: {
                             b.classList.add("child-path-lv-4")
                             break
                         }
@@ -87,20 +86,14 @@ function Post (props: any) {
             })
             .catch(e => console.error(e))
 
-        // active content
-        document.addEventListener("scroll", onscrollHandle)
     }, [])
-
-    let onscrollHandle = () => {
-        
-    }
 
     return (
         <div>
             <Row>
                 {/*content*/}
                 <Col className={'col-md-1 text-left'}>
-                    <Avatar githubHandle="sitebase" size="50" round={true} className={'social-sharing-avt'} />
+                    <Avatar githubHandle="sitebase" size="50" round={true} className={'social-sharing-avt'}/>
                     <div data-v-1b6678dc=""
                          className="mt-3 social-sharing mb-2 social-sharing--horizontal social-sharing--small"><a
                         data-v-1b6678dc="" tooltip-placement="right" rel="noopener" className="link link--muted"
@@ -110,12 +103,12 @@ function Post (props: any) {
                         <a data-v-1b6678dc="" tooltip-placement="right" rel="noopener" className="link link--muted"
                            data-tippy="" data-original-title="Share a link to this page on Twitter"><i
                             data-v-1b6678dc="" aria-hidden="true" className="fa fa-twitter"></i></a>
-                        </div>
+                    </div>
                 </Col>
                 {/*post*/}
                 <Col className={'col-md-8 text-left content-area'}>
                     {/*USER INFO*/}
-                    <div className={'ml-5'}><Avatar githubHandle="sitebase" size="50" round={true} /> {userInfo}</div>
+                    <div className={'ml-5'}><Avatar githubHandle="sitebase" size="50" round={true}/> {userInfo}</div>
                     {/*Title*/}
                     <div className={'mt-3 font-weight-bolder'}>
                         <h2>{title}</h2>
@@ -128,7 +121,8 @@ function Post (props: any) {
                         </Row>
                     </div>
                     {/*Content*/}
-                    <MDEditor.Markdown allowDangerousHtml={true} source={content2}></MDEditor.Markdown>
+                    <MDEditor.Markdown className={'content-markdown'} allowDangerousHtml={true}
+                                       source={content2}></MDEditor.Markdown>
                     {/*<MarkdownView*/}
                     {/*    markdown={content}*/}
                     {/*    options={{ tables: true, emoji: true }}*/}
@@ -138,22 +132,135 @@ function Post (props: any) {
                 </Col>
 
                 {/*index*/}
-                <Col className={`col-md-3`} >
+                <Col className={`col-md-3`}>
                     {/*table of content*/}
-                    <div className={`sidebar-wrapper`} ref={ref} >
+                    <div className={`sidebar-wrapper`} ref={ref}>
                         <div className={`sticky-sidebar post-index hidden-sm-down mt-5 ${isSticky ? 'sticky-tt' : ''}`}>
-                            <div className="section-title-line"><h4 className="text-uppercase">
-                                Table of contents
-                            </h4>
-                                <hr className="filler-line" />
+                            <div className="section-title-line">
+                                <div className="mytextdiv">
+                                    <div className="text-uppercase mytexttitle">
+                                        Table of contents
+                                    </div>
+                                    <div className="divider"></div>
+                                </div>
                             </div>
-                            <div dangerouslySetInnerHTML={{ __html: tocHTML }} />
+                            <div dangerouslySetInnerHTML={{__html: tocHTML}}/>
+
+                            {/*series post*/}
+                            <div className="section-title">
+                                {/*<h4 className="text-uppercase size-13">*/}
+                                {/*Suggested Organizations*/}
+                                {/*</h4>*/}
+                                <div className="mytextdiv mb-3">
+                                    <div className="text-uppercase mytexttitle">
+                                        Suggested Organizations
+                                    </div>
+                                    <div className="divider"></div>
+                                </div>
+
+                                <div className="user-list-container">
+                                    <div className="organization-container">
+                                        <div className="sidebar__organization">
+                                            <div className="sidebar__organization__row"><a href="/o/php-education-team"
+                                                                                           className="d-flex"><img
+                                                width={40}
+                                                height={40}
+                                                data-v-5e990434=""
+                                                src="https://images.viblo.asia/thumbnail/007f6014-5780-4452-828c-f6f6382fdcdd.png"
+                                                srcSet="https://images.viblo.asia/thumbnail-retina/007f6014-5780-4452-828c-f6f6382fdcdd.png  2x"
+                                                alt="Avatar"
+                                                className="avatar avatar--xl avatar--rect avatar--ognzt"/></a>
+                                                <div className="d-flex flex-column justify-content-center"><a
+                                                    href="/o/php-education-team" className="word-break">
+                                                    PHP Education Team
+                                                </a>
+                                                    <div className="stats text-nowrap"><span
+                                                        className="stats-item text-muted" data-tippy=""
+                                                        data-original-title="Posts: 7"><i aria-hidden="true"
+                                                                                          className="stats-item__icon fa fa-pencil"></i>
+        7
+    </span><span className="stats-item text-muted" data-tippy="" data-original-title="Members: 41"><i aria-hidden="true"
+                                                                                                      className="stats-item__icon fa fa-users"></i>
+        41
+    </span><span className="stats-item text-muted" data-tippy="" data-original-title="Followers: 241"><i
+                                                        aria-hidden="true"
+                                                        className="stats-item__icon fa fa-user-plus"></i>
+        241
+    </span></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="organization-container">
+                                        <div className="sidebar__organization">
+                                            <div className="sidebar__organization__row"><a
+                                                href="/o/sun-cyber-security-team" className="d-flex"><img
+                                                width={40}
+                                                height={40}
+                                                data-v-5e990434=""
+                                                src="https://images.viblo.asia/thumbnail/6fba9e20-cbb1-4b63-aa36-c0e5664d19d1.png"
+                                                srcSet="https://images.viblo.asia/thumbnail-retina/6fba9e20-cbb1-4b63-aa36-c0e5664d19d1.png  2x"
+                                                alt="Avatar"
+                                                className="avatar avatar--xl avatar--rect avatar--ognzt"/></a>
+                                                <div className="d-flex flex-column justify-content-center"><a
+                                                    href="/o/sun-cyber-security-team" className="word-break">
+                                                    Sun* Cyber Security Team
+                                                </a>
+                                                    <div className="stats text-nowrap"><span
+                                                        className="stats-item text-muted" data-tippy=""
+                                                        data-original-title="Posts: 139"><i aria-hidden="true"
+                                                                                            className="stats-item__icon fa fa-pencil"></i>
+        139
+    </span><span className="stats-item text-muted" data-tippy="" data-original-title="Members: 10"><i aria-hidden="true"
+                                                                                                      className="stats-item__icon fa fa-users"></i>
+        10
+    </span><span className="stats-item text-muted" data-tippy="" data-original-title="Followers: 196"><i
+                                                        aria-hidden="true"
+                                                        className="stats-item__icon fa fa-user-plus"></i>
+        196
+    </span></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="organization-container">
+                                        <div className="sidebar__organization">
+                                            <div className="sidebar__organization__row"><a href="/o/acb-developer"
+                                                                                           className="d-flex"><img
+                                                width={40}
+                                                height={40}
+                                                data-v-5e990434=""
+                                                src="https://images.viblo.asia/thumbnail/041f2205-6a99-4204-ba36-2863efc8e0e7.png"
+                                                srcSet="https://images.viblo.asia/thumbnail-retina/041f2205-6a99-4204-ba36-2863efc8e0e7.png  2x"
+                                                alt="Avatar"
+                                                className="avatar avatar--xl avatar--rect avatar--ognzt"/></a>
+                                                <div className="d-flex flex-column justify-content-center"><a
+                                                    href="/o/acb-developer" className="word-break">
+                                                    ACB Developer
+                                                </a>
+                                                    <div className="stats text-nowrap"><span
+                                                        className="stats-item text-muted" data-tippy=""
+                                                        data-original-title="Posts: 1"><i aria-hidden="true"
+                                                                                          className="stats-item__icon fa fa-pencil"></i>
+        1
+    </span><span className="stats-item text-muted" data-tippy="" data-original-title="Members: 4"><i aria-hidden="true"
+                                                                                                     className="stats-item__icon fa fa-users"></i>
+        4
+    </span><span className="stats-item text-muted" data-tippy="" data-original-title="Followers: 16"><i
+                                                        aria-hidden="true"
+                                                        className="stats-item__icon fa fa-user-plus"></i>
+        16
+    </span></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {/*suggested orsganization*/}
+                            <div></div>
                         </div>
                     </div>
-                    {/*series post*/}
-                    <div></div>
-                    {/*suggested organization*/}
-                    <div></div>
                 </Col>
             </Row>
         </div>
