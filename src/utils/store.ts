50a10ac794ac788ed9ae0a30e9bfff0025b072b0
Cuongtq37 <trancuong.02092000@gmail.com>
2021-03-03 09:34:57 +0700

    add user id, avatar to store

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -3,8 +3,14 @@ import {createContext} from "react";
 interface Store {
     loggedIn: boolean;
     username: string;
+    name: string;
+    userId: number;
+    setUserId: any;
     setUsername: any;
     setLoggedIn: any;
+    avatar: string;
+    setAvatar: any;
+    setName: any;
 }
 
 export interface StoreContextData {
@@ -23,8 +29,14 @@ export const storeContextDefaultValue: any = {
     store: {
         loggedIn: false,
         username: '',
+        userId: 0,
+        setUserId: '',
         setUsername: '',
-        setLoggedIn: ''
+        setLoggedIn: '',
+        avatar: '',
+        setAvatar: '',
+        name: '',
+        setName: ''
     }
 }
 
